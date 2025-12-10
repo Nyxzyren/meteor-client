69d1f3a5f25053cef1aec1e6a1cde8840df1b755
Nyx <154056739+Nyxzyren@users.noreply.github.com>
2025-12-11 07:00:53 +1100

    feather client

--- src/main/java/meteordevelopment/meteorclient/utils/misc/input/KeyBinds.java
+++ src/main/java/meteordevelopment/meteorclient/utils/misc/input/KeyBinds.java
@@ -13,7 +13,7 @@ import org.lwjgl.glfw.GLFW;
 public class KeyBinds {
     private static final KeyBinding.Category CATEGORY = KeyBinding.Category.create(MeteorClient.identifier("meteor-client"));
 
-    public static KeyBinding OPEN_GUI = new KeyBinding("key.meteor-client.open-gui", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_RIGHT_SHIFT, CATEGORY);
+    public static KeyBinding OPEN_GUI = new KeyBinding("key.meteor-client.open-gui", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_GRAVE_ACCENT, CATEGORY);
     public static KeyBinding OPEN_COMMANDS = new KeyBinding("key.meteor-client.open-commands", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_PERIOD, CATEGORY);
 
     private KeyBinds() {
